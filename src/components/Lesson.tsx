8acd09dfd6026cdf2adad55baa23900cc91bde7f
Rodrigo de Oliveira Carvalho Soares <rodrigo.soares@expocaccer.com.br>
2022-07-02 11:48:35 -0300

    FEAT: Commit inicial

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,5 @@
+export default function Lesson(){
+  return (
+    <div>Lesson</div>
+  )
+}
---
+++ src/pages/Event.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Lesson from "../components/Lesson";
+import Sidebar from "../components/Sidebar";
+import Video from "../components/Video";
+
+export default function Event(){
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <Video />
+      <Lesson />
+    </>
+  )
+}
